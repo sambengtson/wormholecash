4cceb8dbf58604a2b3b04055c2fd407fff853f8f
Chris Troutner <chris.troutner@gmail.com>
2018-09-02 09:22:14 -0700

    token-balance working

--- examples/token-balance/token-balance.js
+++ examples/token-balance/token-balance.js
@@ -5,17 +5,32 @@
 
 "use strict";
 
+// Instantiate wormholecash
 let Wormhole = require("wormholecash/lib/Wormhole").default;
 let wormhole = new Wormhole({ restURL: `http://localhost:3000/v1/` });
 //let wormhole = new Wormhole({ restURL: `https://trest.bitcoin.com/v1/` });
 
-async function pingNode() {
+// Open the wallet generated with create-wallet.
+let walletInfo;
+try {
+  walletInfo = require(`../create-wallet/wallet.json`);
+} catch (err) {
+  console.log(
+    `Could not open wallet.json. Generate a wallet with create-wallet first.
+    Exiting.`
+  );
+  process.exit(0);
+}
+
+// Retrieve the token balance.
+async function tokenBalance() {
   try {
-    const result = await wormhole.DataRetrieval.info();
-    console.log(`Ping succeeded: `);
-    console.log(result);
+    const result = await wormhole.DataRetrieval.balancesForAddress(
+      walletInfo.cashAddress
+    );
+    console.log(JSON.stringify(result, null, 2));
   } catch (err) {
     console.log(err);
   }
 }
-pingNode();
+tokenBalance();
