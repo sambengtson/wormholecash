bddc3d563e949a5dc6e631de870e2e2affd2b87e
Gabriel Cardona <gabriel@bitcoin.com>
2018-09-19 21:54:31 -0700

    Payload creation error tests

--- test/payloadCreation.js
+++ test/payloadCreation.js
@@ -15,6 +15,15 @@ describe("#PayloadCreation", () => {
         assert.equal(burnBCH, "00000044")
       } catch (error) {}
     })
+
+    it(`should fail`, async () => {
+      try {
+        const burnBCH = await Wormhole.PayloadCreation.burnBCH("fail")
+      } catch (error) {
+        assert.equal(error.code, -1)
+        assert.equal(error.message, "JSON value is not an integer as expected")
+      }
+    })
   })
 
   describe("#changeIssuer", () => {
@@ -23,6 +32,15 @@ describe("#PayloadCreation", () => {
         const changeIssuer = await Wormhole.PayloadCreation.changeIssuer(3)
         assert.equal(changeIssuer, "0000004600000003")
       } catch (error) {}
+    })
+
+    it(`should fail`, async () => {
+      try {
+        const changeIssuer = await Wormhole.PayloadCreation.changeIssuer("fail")
+      } catch (error) {
+        assert.equal(error.code, -1)
+        assert.equal(error.message, "JSON value is not an integer as expected")
+      }
     })
   })
 
@@ -33,6 +51,17 @@ describe("#PayloadCreation", () => {
         assert.equal(closeCrowdSale, "0000003500000046")
       } catch (error) {}
     })
+
+    it(`should fail`, async () => {
+      try {
+        const closeCrowdSale = await Wormhole.PayloadCreation.closeCrowdSale(
+          "fail"
+        )
+      } catch (error) {
+        assert.equal(error.code, -1)
+        assert.equal(error.message, "JSON value is not an integer as expected")
+      }
+    })
   })
 
   describe("#grant", () => {
@@ -41,6 +70,15 @@ describe("#PayloadCreation", () => {
         const grant = await Wormhole.PayloadCreation.grant(3, "7000")
         assert.equal(grant, "00000037000000030000000000001b5800")
       } catch (error) {}
+    })
+
+    it(`should fail`, async () => {
+      try {
+        const grant = await Wormhole.PayloadCreation.grant("fail")
+      } catch (error) {
+        assert.equal(error.code, -1)
+        assert.equal(error.message, "JSON value is not an integer as expected")
+      }
     })
   })
 
@@ -69,6 +107,15 @@ describe("#PayloadCreation", () => {
         )
       } catch (error) {}
     })
+
+    it(`should fail`, async () => {
+      try {
+        const crowdsale = await Wormhole.PayloadCreation.crowdsale()
+      } catch (error) {
+        assert.equal(error.code, -1)
+        assert.equal(error.message, "JSON value is not an integer as expected")
+      }
+    })
   })
 
   describe("#fixed", () => {
@@ -91,6 +138,15 @@ describe("#PayloadCreation", () => {
         )
       } catch (error) {}
     })
+
+    it(`should fail`, async () => {
+      try {
+        const fixed = await Wormhole.PayloadCreation.fixed()
+      } catch (error) {
+        assert.equal(error.code, -1)
+        assert.equal(error.message, "JSON value is not an integer as expected")
+      }
+    })
   })
 
   describe("#managed", () => {
@@ -112,6 +168,15 @@ describe("#PayloadCreation", () => {
         )
       } catch (error) {}
     })
+
+    it(`should fail`, async () => {
+      try {
+        const managed = await Wormhole.PayloadCreation.managed()
+      } catch (error) {
+        assert.equal(error.code, -1)
+        assert.equal(error.message, "JSON value is not an integer as expected")
+      }
+    })
   })
 
   describe("#participateCrowdSale", () => {
@@ -122,6 +187,15 @@ describe("#PayloadCreation", () => {
         )
         assert.equal(participateCrowdSale, "000000010000000100000002540be400")
       } catch (error) {}
+    })
+
+    it(`should fail`, async () => {
+      try {
+        const participateCrowdSale = await Wormhole.PayloadCreation.participateCrowdSale()
+      } catch (error) {
+        assert.equal(error.code, -3)
+        assert.equal(error.message, "Invalid amount")
+      }
     })
   })
 
@@ -135,6 +209,15 @@ describe("#PayloadCreation", () => {
         )
       } catch (error) {}
     })
+
+    it(`should fail`, async () => {
+      try {
+        const revoke = await Wormhole.PayloadCreation.revoke("fail")
+      } catch (error) {
+        assert.equal(error.code, -1)
+        assert.equal(error.message, "JSON value is not an integer as expected")
+      }
+    })
   })
 
   describe("#simpleSend", () => {
@@ -144,6 +227,15 @@ describe("#PayloadCreation", () => {
         assert.equal(simpleSend, "000000000000000100000002540be400")
       } catch (error) {}
     })
+
+    it(`should fail`, async () => {
+      try {
+        const simpleSend = await Wormhole.PayloadCreation.simpleSend("fail")
+      } catch (error) {
+        assert.equal(error.code, -1)
+        assert.equal(error.message, "JSON value is not an integer as expected")
+      }
+    })
   })
 
   describe("#STO", () => {
@@ -152,6 +244,15 @@ describe("#PayloadCreation", () => {
         const STO = await Wormhole.PayloadCreation.STO(3, "5000")
         assert.equal(STO, "0000000300000003000000000000138800000003")
       } catch (error) {}
+    })
+
+    it(`should fail`, async () => {
+      try {
+        const STO = await Wormhole.PayloadCreation.STO("fail")
+      } catch (error) {
+        assert.equal(error.code, -1)
+        assert.equal(error.message, "JSON value is not an integer as expected")
+      }
     })
   })
 })
