1558cf7d62352b798c27badb3719b2d407f1e764
Chris Troutner <chris.troutner@gmail.com>
2018-10-04 09:29:11 -0700

    Updated send-tokens example

--- examples/send-tokens/send-tokens.js
+++ examples/send-tokens/send-tokens.js
@@ -2,17 +2,10 @@
   Send existing tokens to another address.
 */
 
-"use strict"
-
 // Instantiate wormholecash
-//let Wormhole = require("wormholecash/lib/Wormhole").default;
 const WH = require("wormholecash/lib/Wormhole").default
-const Wormhole = new WH({
-  restURL: `https://wormholecash-staging.herokuapp.com/v1/`
-})
-
-const BITBOXCli = require("bitbox-cli/lib/bitbox-cli").default
-const BITBOX = new BITBOXCli({ restURL: "https://trest.bitcoin.com/v1/" })
+//const Wormhole = new WH({ restURL: `https://trest.bitcoin.com/v1/` })
+const Wormhole = new WH({ restURL: `https://trest.christroutner.com/v1/` })
 
 // Open the wallet generated with create-wallet.
 let walletInfo
@@ -28,7 +21,7 @@ try {
 
 // Change these values to match your token.
 const RECV_ADDR = ""
-const propertyId = 229 // WH ID identifying the token.
+const propertyId = 1 // WH ID identifying the token. 1 === WHC.
 const TOKEN_QTY = 1 // Number of tokens to send.
 
 // Issue new tokens.
@@ -48,8 +41,7 @@ async function sendTokens() {
     const change = Wormhole.HDNode.derivePath(account, "0/0")
 
     // get the cash address
-    const cashAddress = BITBOX.HDNode.toCashAddress(change)
-    // const cashAddress = walletInfo.cashAddress;
+    const cashAddress = Wormhole.HDNode.toCashAddress(change)
 
     // Create simple send payload.
     const payload = await Wormhole.PayloadCreation.simpleSend(
@@ -58,10 +50,8 @@ async function sendTokens() {
     )
 
     // Get a utxo to use for this transaction.
-    const u = await BITBOX.Address.utxo([cashAddress])
-    console.log(u)
+    const u = await Wormhole.Address.utxo([cashAddress])
     const utxo = findBiggestUtxo(u[0])
-    // const utxo = u[0][1]
 
     // Create a rawTx using the largest utxo in the wallet.
     utxo.value = utxo.amount
@@ -91,10 +81,9 @@ async function sendTokens() {
     tb.sign(0, keyPair, redeemScript, 0x01, utxo.satoshis)
     const builtTx = tb.build()
     const txHex = builtTx.toHex()
-    //console.log(txHex);
 
     // sendRawTransaction to running BCH node
-    const broadcast = await BITBOX.RawTransactions.sendRawTransaction(txHex)
+    const broadcast = await Wormhole.RawTransactions.sendRawTransaction(txHex)
 
     console.log(`You can monitor the below transaction ID on a block explorer.`)
     console.log(`Transaction ID: ${broadcast}`)
