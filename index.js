0731f275e90d22ee4b16cadde7d2a20b5eb092be
Gabriel Cardona <gabriel@bitcoin.com>
2018-10-08 15:55:46 -0700

    Rename to wormhole-sdk and bump to v1.0.0!

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const Wormhole = require("./lib/Wormhole").default
 const clone = require("git-clone")
 const cmd = require("node-cmd")
 
-program.version("0.8.2", "-v, --version")
+program.version("1.0.0", "-v, --version")
 
 program
   .command("new <name>")
