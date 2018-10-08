14fcf39d11af82a848832d65065c9f85c14f04b7
Chris Troutner <chris.troutner@gmail.com>
2018-10-08 10:53:40 -0700

    Starting to work on end-to-end tests

---
+++ test/end-to-end/fixed-token.js
@@ -0,0 +1,28 @@
+/*
+  This is an end-to-end test which verified the happy-path of creating and
+  sending a fixed token.
+
+  This program expects two wallets. Wallet 1 must have at least 1WHC and 10,0000
+  satoshis. Wallet 2 is the recieving wallet. These are wallet.json files generated
+  by the create-wallet example app.
+*/
+
+// Set NETWORK to either testnet or mainnet
+const NETWORK = `testnet`
+
+// The main test function.
+async function fixedTokenTest() {
+  try {
+    // Open wallet 1.
+    // Verify wallet has 1 WHC
+    // Verify wallet has at least 10000 satoshis
+    // Create token
+    // Wait for 1-conf
+    // Send tokens to wallet 2.
+    // Wait for 1-conf
+    // Verify wallet 2 has tokens.
+  } catch (err) {
+    console.log(`Error in fixedTokenTest: `, err)
+  }
+}
+fixedTokenTest()
